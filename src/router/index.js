a2e19ded0268fcf5ca61954316010d6fca36e876
xulinSky <380074137@qq.com>
2025-11-11 19:41:41 +0800

    Add Vue Router configuration for Eletron app

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+// 路由配置
+const routes = [
+  // 设置 Eletron 为首页
+  {
+    path: '/',
+    name: 'EletronHome',
+    component: () => import('../views/Eletron.vue'),
+    props: true
+  },
+  {
+    path: '/eletron/:fileId?',
+    name: 'Eletron',
+    component: () => import('../views/Eletron.vue'),
+    props: true
+  },
+  {
+    path: '/files',
+    name: 'FileList',
+    component: () => import('../views/FileList.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+export default router;
+
